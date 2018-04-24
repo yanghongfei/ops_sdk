5c27674bae3a0f69f975053b02aa7ba115e8472f
沈硕 <shenshuo@shinezone.com>
2018-04-24 18:22:36 +0800

    修改依赖

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ from distutils.core import setup
 setup(
     name='opssdk',
     version='0.0.3',
-    packages=['opssdk', 'opssdk.logs', 'opssdk.operate', 'opssdk.install','opssdk.get_info','opssdk.utils','websdk'],
+    packages=['opssdk', 'opssdk.logs', 'opssdk.operate', 'opssdk.install', 'opssdk.get_info', 'opssdk.utils', 'websdk'],
     url='http://baidu.com',
     license='',
-    install_requires=['fire==0.1.2', 'shortuuid==0.5.0', 'pycrypto==2.6.1', 'PyMySQL==0.7.11', 'salt==2017.7.2',
-                      'requests==2.18.4'],
+    install_requires=['fire==0.1.2', 'shortuuid==0.5.0', 'pycrypto==2.6.1', 'PyMySQL==0.7.11', 'requests==2.18.4'],
     author='shenshuo',
     author_email='191715030@qq.com',
     description='SDK of the operation and maintenance script'
