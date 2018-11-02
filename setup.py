8c2387001ab70c3aa284d345a4efa94d62511056
沈硕 <shenshuo@shinezone.com>
2018-11-02 16:16:38 +0800

    更新依赖

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
-'''
+""""
 author : shenshuo
 date   : 2018年2月5日13:37:54
-role   : 运维SDK
-'''
+desc   : 运维SDK
+update : 添加tornado SDK
+"""
 from distutils.core import setup
 
 setup(
     name='opssdk',
-    version='0.0.5',
+    version='0.0.6',
     packages=['opssdk', 'opssdk.logs', 'opssdk.operate', 'opssdk.install', 'opssdk.get_info', 'opssdk.utils', 'websdk'],
-    url='http://baidu.com',
+    url='https://github.com/ss1917/ops_sdk/',
     license='',
-    install_requires=['fire', 'shortuuid===0.5.0', 'Crypto===1.4.1', 'PyMySQL==0.7.11', 'requests==2.18.4','tornado===5.0'],
+    install_requires=['fire', 'shortuuid', 'Crypto===1.4.1', 'PyMySQL==0.7.11', 'requests','tornado===5.0'],
     author='shenshuo',
     author_email='191715030@qq.com',
     description='SDK of the operation and maintenance script'
